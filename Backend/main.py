24966a67fb0687a5efe64a7afb441f7abc8e1616
akshata <akshatajadhav7902@gmail.com>
2026-02-22 20:20:03 +0530

    .

--- Backend/main.py
+++ Backend/main.py
@@ -109,9 +109,9 @@ def add_fruits(fruit:Fruit):
 
 
 # To check api deployement successful
-@app.get("/AI")
+@app.get("/")
 async def health_care():
     return "The Health check is successful!"
 
-if __name__ =="__main__":
-    uvicorn.run(app,host="0.0.0.0",port=10000)
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
